e016260e56b655c30f727e08cd25d872649f501f
ShailjaShailja <46799353+ShailjaShailja@users.noreply.github.com>
2022-11-02 14:55:29 -0700

    Update reeb_graphml.py

--- reeb_graphml.py
+++ reeb_graphml.py
@@ -1,10 +1,14 @@
+"""
+This code is used to compute the Reeb graphs for all the CC trajectories and save the graphs as .gpickle format.
+Please modify the path names to compute Reeb graphs for all the streamlines in the given folder.
+"""
 import nibabel as nib
 import os
 import robustReebConstruction_avg_loc_qb as rc
 import networkx as nx
 import pickle
 #path name for CC files
-trk_file_path = "/media/hdd2/shailja/Crash/CC_ref/"
+trk_file_path = "/media/hdd2/shailja/Crash/CC_ref/" #specify the folder that has all the streamlines file in .trk or .trk.gz format
 trk_files = os.listdir(trk_file_path)
 for file in trk_files:
     if ".trk.gz" in file and ".txt" not in file:
@@ -12,13 +16,11 @@ for file in trk_files:
         cc_streamlines =  nib.streamlines.load(trk_file_path+file)
         print("n = ",len(cc_streamlines.streamlines))        
         eps =2.5
-        delta = 3
-        tau = 5
-        H, node_loc,cluster_edge = rc.constructRobustReeb(cc_streamlines.streamlines, eps, delta, tau)
+        alpha = 3
+        delta = 5
+        H, node_loc,cluster_edge = rc.constructRobustReeb(cc_streamlines.streamlines, eps, alpha, delta)
         #write H
         nx.write_gpickle(H, trk_file_path+"H_CC_ref_"+file.split("_")[0]+"_"+file.split("_")[1]+".gpickle")
         #save node_loc dict
         with open(trk_file_path+"node_loc_CC_ref_"+file.split("_")[0]+"_"+file.split("_")[1]+".pickle", 'wb') as handle:
             pickle.dump(node_loc, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        
